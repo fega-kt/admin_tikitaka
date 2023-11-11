c1c78109b10703c068b1fd0ce740e025af580d4b
Kim Thai <fega.kt@gmail.com>
2023-11-11 22:48:31 +0700

    icon svg

---
+++ src/utils/icon.tsx
@@ -0,0 +1,61 @@
+export const iconError = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    x="0px"
+    y="0px"
+    width="20"
+    height="20"
+    viewBox="0,0,256,256"
+  >
+    <g
+      fill="none"
+      fillRule="nonzero"
+      stroke="none"
+      strokeWidth="1"
+      strokeLinecap="butt"
+      strokeLinejoin="miter"
+      strokeMiterlimit="10"
+      strokeDasharray=""
+      strokeDashoffset="0"
+      fontFamily="none"
+      fontWeight="none"
+      fontSize="none"
+      textAnchor="none"
+    >
+      <g transform="scale(5.33333,5.33333)">
+        <path
+          d="M44,24c0,11.045 -8.955,20 -20,20c-11.045,0 -20,-8.955 -20,-20c0,-11.045 8.955,-20 20,-20c11.045,0 20,8.955 20,20z"
+          fill="#f44336"
+        ></path>
+        <path
+          d="M29.656,15.516l2.828,2.828l-14.14,14.14l-2.828,-2.828z"
+          fill="#ffffff"
+        ></path>
+        <path
+          d="M32.484,29.656l-2.828,2.828l-14.14,-14.14l2.828,-2.828z"
+          fill="#ffffff"
+        ></path>
+      </g>
+    </g>
+  </svg>
+);
+
+export const iconSuccess = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    x="0px"
+    y="0px"
+    width="20"
+    height="20"
+    viewBox="0 0 48 48"
+  >
+    <path
+      fill="#4caf50"
+      d="M44,24c0,11.045-8.955,20-20,20S4,35.045,4,24S12.955,4,24,4S44,12.955,44,24z"
+    ></path>
+    <path
+      fill="#ccff90"
+      d="M34.602,14.602L21,28.199l-5.602-5.598l-2.797,2.797L21,33.801l16.398-16.402L34.602,14.602z"
+    ></path>
+  </svg>
+);
