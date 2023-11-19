ab03a2e82a13cba6d7994c23770a290af31f3337
Kim Thai <fega.kt@gmail.com>
2023-11-19 19:08:48 +0700

    update config taildwincdd

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -19,13 +19,6 @@ export default {
         acc: '#007ACC',
       },
     },
-    screens: {
-      sm: '576px',
-      md: '768px',
-      lg: '992px',
-      xl: '1200px',
-      xxl: '1400px',
-    },
   },
   plugins: [],
 };
